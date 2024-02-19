30536a6b56a7c90f03f3329a859eab49325bb09b
wolfy <a.samartino@protonmail.com>
2024-02-19 14:52:08 +0100

    started working on chatper 3 level 1

--- src/chapter_3/chapter_3_level_1.rs
+++ src/chapter_3/chapter_3_level_1.rs
@@ -6,19 +6,24 @@
 //////                                     Data Types                                      //////
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-// reverse the tuple provided using destructuring and return the new tuple
-pub fn reverse_tuple(tup: (u32, u32, u32)) -> (u32, u32, u32) {
+// reverse the tuple provided and return the new tuple, f.i. (1,2,3,4) -> (4,3,2,1)
+pub fn reverse_tuple(tup: (u32, u32, u32, u32)) -> (u32, u32, u32, u32) {
     todo!()
 }
 
-// reverse the tuple provided using destructuring and return the new tuple using shadowing
-pub fn reverse_tuple(tup: (u32, u32, u32)) -> (u32, u32, u32) {
+// reverse the tuple provided using destructuring and return the new tuple using shadowing, , f.i. (1,2,3,4) -> (4,3,2,1)
+pub fn reverse_tuple_destructuring(tup: (u32, u32, u32)) -> (u32, u32, u32) {
     let response = tup;
     todo!()
 }
 
 // this function return an array where every element of the array provided as argument is incremented by 10
 pub fn array_add_10(arr: [u32; 5]) -> [u32; 5] {
+    todo!()
+}
+
+// reverse the array provided and return the result, f.i. [1,2,3,4,5] -> [5,4,3,2,1]
+pub fn reverse_array(arr: [u32; 5]) -> [u32; 5] {
     todo!()
 }
 
@@ -40,4 +45,28 @@ pub fn is_divisible_by_two_three_five(number: u32) -> u32 {
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    // Data Types
+    #[test]
+    fn test_reverse_tuple() {
+        let tup: (u32,u32,u32,u32) = (4,5,6,7);
+        assert_eq!(tup.rev(), reverse_tuple(tup));
+    }
+
+    #[test]
+    fn test_reverse_tuple_destructuring() {
+        let tup: (u32,u32,u32,u32) = (4,5,6,7);
+        assert_eq!(tup.rev(), reverse_tuple(tup));
+    }
+
+    #[test]
+    fn test_array_add_10() {
+        let arr: [u32;5] = [0,1,2,3,4];
+        let response: [u32;5] = [10,11,12,13,14];
+        assert_eq!(response,_array_add_10(arr));
+    }
+
+}
